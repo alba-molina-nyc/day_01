ee1d01478adb505ced78503b848f191da9dd3c32
alba-molina-nyc <avmanyc@gmail.com>
2022-08-14 15:17:12 -0400

    writes note for typescript

--- day_02/tip_calculator.py
+++ day_02/tip_calculator.py
@@ -10,6 +10,5 @@ converted_split = int(even_split)
 print(f'each person pays {converted_split}, you your POST TIP TOTAL is {post_tip_total}, you would like to SPLIT this bill among {num_people} you would like to leave a {tip_percentage} PERCENT on a bill TOTAL of: {pre_tip_total}')
 
 
-
-
+# TODO figure out how to use typescript to send it to an html file
 
