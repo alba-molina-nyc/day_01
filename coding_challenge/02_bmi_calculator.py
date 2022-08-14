0d8bd4961dfaecf4d5423cfc027518a583c979d7
alba-molina-nyc <avmanyc@gmail.com>
2022-08-13 22:18:24 -0400

    done

--- coding_challenge/02_bmi_calculator.py
+++ coding_challenge/02_bmi_calculator.py
@@ -1,8 +1,10 @@
 def bmi_calculator():
     height = input('please enter your height in m: ')
     weight = input('please enter your weight in kg: ')
-    bmi = int(weight) / int(height) ** 2
+    bmi = int(weight) / float(height) ** 2
+    bmi_as_int = int(bmi)
     print(f'your bmi is {bmi}')
+    print(f'your bmi as int is {bmi_as_int}')
 
 
 if __name__ == '__main__':
